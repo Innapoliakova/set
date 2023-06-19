242562ad28e0ae781c2aedd03f0ca8179b699dd0
Inna Poliakova <polyakova1081@gmail.com>
2023-06-19 18:07:56 +0100

    component Header added

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import { Route, Routes } from "react-router-dom";
+// import { Route, Routes } from "react-router-dom";
 
-import About from "./pages/About";
-import Home from "./pages/Home";
-
-const App = () => (
-	<Routes>
-		<Route path="/" element={<Home />} />
-		<Route path="/about/this/site" element={<About />} />
-	</Routes>
-);
+// import About from "./pages/About";
+// import Home from "./pages/Home";
+import Header
+ from "./components/Header";
+function App () {
+	return (
+		<div className="App">
+			<Header />
+		</div>
+	)
+	// <Routes>
+	// 	<Route path="/" element={<Home />} />
+	// 	<Route path="/about/this/site" element={<About />} />
+	// </Routes>
+};
 
 export default App;
---
+++ client/src/components/Header.js
@@ -0,0 +1,12 @@
+
+const Header = () => {
+    return (
+        <header>
+            <div className="header-section">
+                <h1>CYF's picture gallery</h1>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
