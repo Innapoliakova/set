db338beea288687e3e1d1a2c9bdd6ec4724794e5
roozbeh <108986775+Roozbehshokri1990@users.noreply.github.com>
2023-06-20 18:57:59 +0100

    change roozbeh

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,7 +1,7 @@
 const Upload = () => {
 	return (
 			<div className="upload-section">
-				<h2>HERE will be form for upload</h2>
+				<h2>HERE will be form for roozbeh upload</h2>
 			</div>
 	);
 };
