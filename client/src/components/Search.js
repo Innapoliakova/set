3cb399d121d08d4d9f4cbc00dd13e98aaa705b38
Inna Poliakova <polyakova1081@gmail.com>
2023-06-20 22:02:16 +0100

    Search.js component created, Search.css created and search.png for search  added

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,13 +1,20 @@
 import SearchIcon from "./search.png";
+import "./Search.css";
 
 const Search = () => {
 	return (
 		<div className="search-section">
-			<div className="search-icon">
-				<img src={SearchIcon} alt="icon for searcing" />
-			</div>
-			<div className="seach-input">
-				<input type="text" className="serch-input" placeholder="Search" />
+			<div className="search-input">
+				<input
+					type="text"
+					className="search-input-field"
+					placeholder="Search"
+				/>
+				<img
+					className="icon-for-search"
+					src={SearchIcon}
+					alt="icon for searching"
+				/>
 			</div>
 		</div>
 	);
