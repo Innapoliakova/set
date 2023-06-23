8b41f41f3b687086d721b2d2f3c6b7d451010cb6
mohamedabdi <alaale7404@gmail.com>
2023-06-23 15:45:39 +0100

    first section of the footer

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,8 +1,42 @@
+import FacebookRoundedIcon from "@mui/icons-material/FacebookRounded";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import EmailRoundedIcon from "@mui/icons-material/EmailRounded";
+
 const Footer = () => {
 	return (
 		<footer>
 			<div className="footer-section">
-				<h1>HERE will be our footer</h1>
+				 <span className="newsletter">Sign up to our newsletter</span>
+        <input
+          type="email"
+          placeholder="Enter your email"
+          className="email-required"
+        />
+        <button className="button">Subscribe</button>
+				<a href="https://www.facebook.com/codeyourfuture.io">
+        <FacebookRoundedIcon className="fb" />
+      </a>
+	   <a href="https://twitter.com/CodeYourFuture">
+          <TwitterIcon className="tw" />
+        </a>
+        <a href="https://www.linkedin.com/company/codeyourfuture/mycompany/">
+          <LinkedInIcon className="link" />
+        </a>
+        <a href="https://www.instagram.com/codeyourfuture_/">
+          <InstagramIcon className="instag" />
+        </a>
+        <a href="https://github.com/CodeYourFuture">
+          <GitHubIcon className="github" />
+        </a>
+        <a href="mailto:london@codeyourfuture.io">
+          <EmailRoundedIcon className="email" />
+        </a>
+
+        
+
 			</div>
 		</footer>
 	);
