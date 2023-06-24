7bc0569a4b197cd9e9decfb51636b2b09b276514
roozbeh <108986775+Roozbehshokri1990@users.noreply.github.com>
2023-06-24 13:20:23 +0100

    change upload.css
    i change my css

--- client/src/components/Upload.js
+++ client/src/components/Upload.js
@@ -1,46 +1,46 @@
 // import React from 'react';
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import "./upload.css";
 const Upload = () => {
-	const handleFileUpload = (event) => {
-		const file = event.target.files[0];
-		
-	  };
+// 	const handleFileUpload = (event) => {
+// 		const file = event.target.files[0];
 
-	  const [input1, setInput1] = useState('description');
-	  const [input2, setInput2] = useState('tags');
-	  const [input3, setInput3] = useState('categories');
-	
-	  const handleInput1Change = (event) => {
+// };
+
+ const [input1, setInput1] = useState("description");
+ const [input2, setInput2] = useState("tags");
+ const [input3, setInput3] = useState("categories");
+
+	const handleInput1Change = (event) => {
 		setInput1(event.target.value);
-	  };
-	
-	  const handleInput2Change = (event) => {
+	};
+
+	const handleInput2Change = (event) => {
 		setInput2(event.target.value);
-	  };
-	
-	  const handleInput3Change = (event) => {
+ };
+ const handleInput3Change = (event) => {
 		setInput3(event.target.value);
-	  };
+ };
 
 
 	return (
-			<div className="upload-section">
-			<input type="file" onChange={handleFileUpload} />
-				<h2>HERE will be form for roozbeh upload</h2>
-	
+			<form className="upload-section" >
+			<input type="file"  />
+
+
 	<div className="container">
 	<div className="input-field">
-	  <input type="text" value={input1} onChange={handleInput1Change} />
+ <input type="text" value={input1} onChange={handleInput1Change} />
 	</div>
 	<div className="input-field">
-	  <input type="text" value={input2} onChange={handleInput2Change} />
+ <input type="text" value={input2} onChange={handleInput2Change} />
 	</div>
 	<div className="input-field">
-	  <input type="text" value={input3} onChange={handleInput3Change} />
+ <input type="text" value={input3} onChange={handleInput3Change} />
 	</div>
   </div>
-    </div>
-	)
+    </form>
+	);
 };
 
 export default Upload;
