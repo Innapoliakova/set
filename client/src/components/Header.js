8ebf917b016e4037ae33bfcaa18821811569da09
Inna Poliakova <polyakova1081@gmail.com>
2023-06-28 17:25:05 +0100

    Login button added to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,13 +2,16 @@ import logo from "../assets/icons/cyf.png";
 import "./Header.css";
 const Header = () => {
     return (
-        <header>
-            <div className="header-section">
-            <img src={logo} alt="Logo" className="logo" />
-                <h1 className="title">CYF's picture gallery</h1>
-            </div>
-        </header>
-    );
+			<header>
+				<div className="header-section">
+					<img src={logo} alt="Logo" className="logo" />
+					<h1 className="title">CYF's picture gallery</h1>
+				</div>
+				<div className="login-section">
+					<button>Login</button>
+				</div>
+			</header>
+		);
 };
 
 export default Header;
