df12390357ab711adfcb586845c33495b0e5965f
Inna Poliakova <polyakova1081@gmail.com>
2023-06-28 17:46:42 +0100

    css style for header after added login button

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,11 +4,15 @@ const Header = () => {
     return (
 			<header>
 				<div className="header-section">
-					<img src={logo} alt="Logo" className="logo" />
-					<h1 className="title">CYF's picture gallery</h1>
-				</div>
-				<div className="login-section">
-					<button>Login</button>
+					<div className="image">
+						<img src={logo} alt="Logo" className="logo" />
+					</div>
+					<div className="title">
+						<h1 className="title">CYF's picture gallery</h1>
+					</div>
+					<div className="login-button">
+						<button>Login</button>
+					</div>
 				</div>
 			</header>
 		);
