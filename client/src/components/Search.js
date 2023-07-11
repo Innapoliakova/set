3e54d375df9b4f26f88ce162f705c6270d8dd6cb
roozbeh <108986775+Roozbehshokri1990@users.noreply.github.com>
2023-07-11 18:46:07 +0100

    +fix bug   +search button

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,46 +1,29 @@
 import "./Search.css";
 import { useState } from "react";
 
-const Search = ({ setSearchQuery }) => {
-	const [searchInput, setSearchInput] = useState("");
-	const [isLoading, setIsLoading] = useState(false);
-	const handleChange = (event) => {
-		setSearchInput(event.target.value.trim());
-	};
+const Search = ({ setSearchQuery, isLoading }) => {
+    const [searchInput, setSearchInput] = useState("");
+        const handleChange = (event) => {
+        setSearchInput(event.target.value.trim());
+    };
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		setIsLoading(true);
-		setSearchQuery(searchInput);
-		try {
-			await setSearchQuery(searchInput); // Assuming setSearchQuery is an asynchronous API call
-			setIsLoading(false);
-		  } catch (error) {
-			console.error("API call error:", error);
-			setIsLoading(false);
-		  }
-	};
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setSearchQuery(searchInput);
+    };
 
-	return (
-		<form className="search-box" onSubmit={handleSubmit}>
+    return (
+        <form className="search-box" onSubmit={handleSubmit}>
         <input type="text" name="" id="" placeholder="search..." onChange={handleChange} />
-        {/*<a href="##" className="icon">
-            <i className="fas fa-search"></i>
-	</a>*/}
-		<a href="##" className="icon">
-		{isLoading ? (
-<i className="fas fa-spinner fa-spin"></i>
-		) : (
-<i className="fas fa-search"></i>
-		)}
- </a>
+ <button type="submit" className="icon" disabled={isLoading}>
+        {isLoading ? (
+          <i className="fas fa-spinner fa-spin"></i>
+        ) : (
+          <i className="fas fa-search"></i>
+        )}
+      </button>
+
     </form>
-	);
+    );
 };
 export default Search;
-
-
-
-
-
-
