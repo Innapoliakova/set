cf0ffa62ab92e3b728b8c2b12245e47708ec1707
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2023-07-11 17:06:10 +0100

    Configure the Auth0Provider component

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,31 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import { Auth0Provider } from "@auth0/auth0-react";
 import App from "./App";
 
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
+
+const domain = "dev-kwwblh1qwdip54ij.uk.auth0.com";
+const clientId = "SsxO9vdIdy1wMTC8iBH1GAL2riST4MpU";
+
+const onRedirectCallback = (appState) => {
+	history.push(
+		appState && appState.returnTo ? appState.returnTo : window.location.pathname
+	);
+};
+
 createRoot(document.getElementById("root")).render(
-	<BrowserRouter>
-		<App />
-	</BrowserRouter>
+	<Auth0Provider
+		domain={domain}
+		clientId={clientId}
+		authorizationParams={{
+			redirect_uri: window.location.origin,
+		}}
+		onRedirectCallback={onRedirectCallback}
+	>
+		<BrowserRouter>
+			<App />
+		</BrowserRouter>
+	</Auth0Provider>
 );
