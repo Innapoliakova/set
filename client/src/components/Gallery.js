3f3ad6569fda9733f6b828d20e2b7392356d0d99
roozbeh <108986775+Roozbehshokri1990@users.noreply.github.com>
2023-07-11 19:20:38 +0100

    fixed - show no images message

--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -3,32 +3,41 @@ import "./Gallery.css";
 import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 
 const Gallery = ({ images, isLogin, message, setUpdateImages }) => {
-	return (
-		<>
-			{message ? (
-				<h1 className="message" data-qa="message">
-					{message}
-				</h1>
-			) : (
-				<div className="gallery-section">
-					<ResponsiveMasonry
-						columnsCountBreakPoints={{ 350: 1, 400: 2, 750: 3, 900: 4 }}
-					>
-						<Masonry gutter="1.5rem">
-							{images.map((image) => (
-								<ImageCard
-									key={image.id}
-									image={image}
-									isLogin={isLogin}
-									setUpdateImages={setUpdateImages}
-								/>
-							))}
-						</Masonry>
-					</ResponsiveMasonry>
-				</div>
-			)}
-		</>
-	);
+
+  return (
+    <>
+      {images.length === 0 ? (
+        <div className="no-images-message">
+          No images were found that match your search/filter.
+        </div>
+      ) : (
+        <>
+          {message ? (
+            <h1 className="message" data-qa="message">
+              {message}
+            </h1>
+          ) : (
+            <div className="gallery-section">
+              <ResponsiveMasonry
+                columnsCountBreakPoints={{ 350: 1, 400: 2, 750: 3, 900: 4 }}
+              >
+                <Masonry gutter="1.5rem">
+                  {images.map((image) => (
+                    <ImageCard
+                      key={image.id}
+                      image={image}
+                      isLogin={isLogin}
+                      setUpdateImages={setUpdateImages}
+                    />
+                  ))}
+                </Masonry>
+              </ResponsiveMasonry>
+            </div>
+          )}
+        </>
+      )}
+    </>
+  );
 };
 
 export default Gallery;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -9,47 +9,51 @@ import Footer from "../components/Footer";
 import Filter from "../components/Filter";
 
 export function Home() {
-	const [message, setMessage] = useState("Loading...");
-	const [images, setImages] = useState([]);
-	const [isLogin, setIsLogin] = useState(true);
-	const [selectedFilter, setSelectedFilter] = useState(null);
-	const [searchQuery, setSearchQuery] = useState("");
-	const [updateImages, setUpdateImages] = useState(true);
+    const [message, setMessage] = useState("Loading...");
+    const [images, setImages] = useState([]);
+    const [isLogin, setIsLogin] = useState(true);
+    const [selectedFilter, setSelectedFilter] = useState(null);
+    const [searchQuery, setSearchQuery] = useState("");
+    const [updateImages, setUpdateImages] = useState(true);
 
-	useEffect(() => {
-		const fetchData = async () => {
-			try {
-				const res = await fetch(
-					`/api/images?filter=${selectedFilter}&searchQuery=${searchQuery}`
-				);
-				if (!res.ok) {
-					throw new Error(res.statusText);
-				}
-				const allImages = await res.json();
-				setMessage(null);
-				setImages(allImages.data);
-			} catch (err) {
-				console.error(err);
-			}
-		};
-		fetchData();
-	}, [updateImages, selectedFilter, searchQuery]);
+    const [isLoading, setIsLoading] = useState(false);
 
-	return (
-		<div className="App">
-			<Header />
-			<Hero setSearchQuery={setSearchQuery} />
-			{/* <Search setSearchQuery={setSearchQuery} /> */}
-			<Filter setSelectedFilter={setSelectedFilter} />
-			<Gallery
-				images={images}
-				isLogin={isLogin}
-				message={message}
-				setUpdateImages={setUpdateImages}
-			/>
-			<Footer />
-		</div>
-	);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                setIsLoading(true);
+                const res = await fetch(
+                    `/api/images?filter=${selectedFilter}&searchQuery=${searchQuery}`
+                );
+                if (!res.ok) {
+                    throw new Error(res.statusText);
+                }
+                const allImages = await res.json();
+                setMessage(null);
+                setImages(allImages.data);
+                setIsLoading(false);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+        fetchData();
+    }, [updateImages, selectedFilter, searchQuery]);
+
+    return (
+        <div className="App">
+            <Header />
+            <Hero setSearchQuery={setSearchQuery} isLoading={isLoading} />
+            {/* <Search setSearchQuery={setSearchQuery} /> */}
+            <Filter setSelectedFilter={setSelectedFilter} />
+            <Gallery
+                images={images}
+                isLogin={isLogin}
+                message={message}
+                setUpdateImages={setUpdateImages}
+            />
+            <Footer />
+        </div>
+    );
 }
 
 export default Home;
