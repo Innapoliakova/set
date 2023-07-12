9141c543486a49dae2438efcc187d3dfa8b7a7ba
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2023-07-12 15:41:22 +0100

    Add route for Profile component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import UploadPage from "./pages/uploadPage";
 import Home from "./pages/Home";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Profile from "./pages/Profile";
 
 function App() {
 	return (
@@ -10,6 +11,7 @@ function App() {
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route path="/upload" element={<UploadPage />} />
+				<Route path="/profile" element={<Profile />} />
 			</Routes>
 
 			<ToastContainer />
