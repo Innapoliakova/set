224f4fe5d9fb9917ea3a9658ee111def96dc7f6f
Mohamed Abdalla <mohamed.e.abdalla1@gmail.com>
2023-07-13 11:08:49 +0100

    Adjust to use process.env in react app

--- client/webpack/common.config.js
+++ client/webpack/common.config.js
@@ -1,4 +1,12 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const dotenv = require("dotenv");
+
+const env = dotenv.config().parsed;
+const envKeys = Object.keys(env).reduce((prev, next) => {
+	prev[`process.env.${next}`] = JSON.stringify(env[next]);
+	return prev;
+}, {});
 
 module.exports = {
 	entry: "./client/src/index.js",
@@ -32,5 +40,6 @@ module.exports = {
 			favicon: "./client/src/favicon.ico",
 			template: "./client/src/index.html",
 		}),
+		new webpack.DefinePlugin(envKeys),
 	],
 };
